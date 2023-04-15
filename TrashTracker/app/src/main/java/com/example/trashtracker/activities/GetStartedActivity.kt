10fc84dc1b83f73be4b7107abb0d3a87eecd8f8e
Ritex12in <riteshsk001@gmail.com>
2023-04-15 19:13:45 +0530

    created sign in activity

--- TrashTracker/app/src/main/java/com/example/trashtracker/activities/GetStartedActivity.kt
+++ TrashTracker/app/src/main/java/com/example/trashtracker/activities/GetStartedActivity.kt
@@ -1,12 +1,20 @@
 package com.example.trashtracker.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.cardview.widget.CardView
 import com.example.trashtracker.R
 
 class GetStartedActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_get_started)
+
+        val getStartBtn: CardView = findViewById(R.id.cvGetStarted)
+        getStartBtn.setOnClickListener {
+            startActivity(Intent(this,SignInActivity::class.java))
+            finish()
+        }
     }
 }
