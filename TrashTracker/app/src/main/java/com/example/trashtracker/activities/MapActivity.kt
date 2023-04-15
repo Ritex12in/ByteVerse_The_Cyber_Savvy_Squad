01f7c5678d7320a787c904f489efe5bf6b8c1317
Ritex12in <riteshsk001@gmail.com>
2023-04-16 01:28:37 +0530

    Android: Adding navigation bar and adding map and settings activity

---
+++ TrashTracker/app/src/main/java/com/example/trashtracker/activities/MapActivity.kt
@@ -0,0 +1,12 @@
+package com.example.trashtracker.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.trashtracker.R
+
+class MapActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_map)
+    }
+}
---
+++ TrashTracker/app/src/main/java/com/example/trashtracker/activities/SettingsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.trashtracker.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.trashtracker.R
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+}
