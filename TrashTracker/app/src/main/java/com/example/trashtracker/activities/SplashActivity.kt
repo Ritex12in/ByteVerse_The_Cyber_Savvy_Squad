605ea953b136cd503f962759682c04393399b315
Ritex12in <riteshsk001@gmail.com>
2023-04-15 17:35:14 +0530

    adding splash screen

--- TrashTracker/app/src/main/java/com/example/trashtracker/activities/SplashActivity.kt
+++ TrashTracker/app/src/main/java/com/example/trashtracker/activities/SplashActivity.kt
@@ -1,11 +1,12 @@
-package com.example.trashtracker
+package com.example.trashtracker.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.trashtracker.R
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash)
     }
 }
