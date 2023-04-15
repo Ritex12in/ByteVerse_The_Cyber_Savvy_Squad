f5a1015a2e4793e36747ee42ef8d96aed371b41c
Ritex12in <riteshsk001@gmail.com>
2023-04-16 02:18:11 +0530

    Android: Added map on map activity

--- TrashTracker/app/src/main/java/com/example/trashtracker/activities/MainActivity.kt
+++ TrashTracker/app/src/main/java/com/example/trashtracker/activities/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.example.trashtracker.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.trashtracker.R
+import com.example.trashtracker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var binding:ActivityMainBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        binding?.btnMap?.setOnClickListener {
+            val intent = Intent(this, MapActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding?.btnSettings?.setOnClickListener {
+            startActivity(Intent(this, SettingsActivity::class.java))
+        }
     }
 }
--- TrashTracker/app/src/main/java/com/example/trashtracker/activities/MapActivity.kt
+++ TrashTracker/app/src/main/java/com/example/trashtracker/activities/MapActivity.kt
@@ -1,12 +1,46 @@
 package com.example.trashtracker.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 import com.example.trashtracker.R
+import com.example.trashtracker.databinding.ActivityMapBinding
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 
-class MapActivity : AppCompatActivity() {
+class MapActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private var binding:ActivityMapBinding? = null
+    private var mGoogleMap: GoogleMap? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_map)
+        binding = ActivityMapBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        val w: Window = window
+        w.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
+
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.mapFragment) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+        binding?.btnSettings?.setOnClickListener {
+            startActivity(Intent(this,SettingsActivity::class.java))
+            finish()
+        }
+        binding?.btnDashboard?.setOnClickListener {
+            finish()
+        }
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mGoogleMap = googleMap
     }
 }
+
