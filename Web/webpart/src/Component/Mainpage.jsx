0ffa3019344d3a25a2e6b023b4acb36f55447522
Shreeyansh Kumar <shreeyanshk.ug21.ec@nitp.ac,in>
2023-04-16 14:56:10 +0530

    WEB-PIE chart showing type of waste added

--- Web/webpart/src/Component/Mainpage.jsx
+++ Web/webpart/src/Component/Mainpage.jsx
@@ -1,20 +1,21 @@
 import React, { Component } from 'react'
 import '../Styles/Mainpage.css'
 import GetMap from './Map'
- class Mainpage extends Component {
+import Donut from './Donut'
+class Mainpage extends Component {
 
   render() {
     return (
       <>
-      < div className="Main">
-          <GetMap/>        
-        <div className="upper"></div>
-        <div className="Lower">
-          <div className="Left_Lower_half"></div>
-          <div className="Right_Lower_half"></div>
+        < div className="Main">
+          <GetMap />
+          <div className="upper"></div>
+          <div className="Lower">
+            <div className="Left_Lower_half"></div>
+            <div className="Right_Lower_half"><Donut /></div>
 
-       
-        </div>
+
+          </div>
         </div>
       </>
     )
